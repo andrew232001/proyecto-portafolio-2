1667ad87dcbd8889bdeadfa452ee119fa2644136
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 01:00:18 -0300

    probando codigo de funcionamiento

--- sistema_de_asistesia_DUOC_UC/settings.py
+++ sistema_de_asistesia_DUOC_UC/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'staticfiles/')] 
 
+#debo borrar este codigo
 CSRF_TRUSTED_ORIGINS = ['https://proyecto-portafolio-2-production.up.railway.app']
