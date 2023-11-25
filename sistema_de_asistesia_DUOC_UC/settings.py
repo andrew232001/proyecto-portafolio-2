0b46193f358e8c1dd9a2a80a8a77a9cbe1df7a19
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 15:12:17 -0300

    primer template

--- sistema_de_asistesia_DUOC_UC/settings.py
+++ sistema_de_asistesia_DUOC_UC/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sistema_de_asistencia_DUOC_UC', 'staticfiles'),]
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
