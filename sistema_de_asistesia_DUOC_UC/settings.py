89cd8019babd7275db484bf8df8b9a36fd2eaffe
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 01:34:51 -0300

    probando codigo de funcionamiento

--- sistema_de_asistesia_DUOC_UC/settings.py
+++ sistema_de_asistesia_DUOC_UC/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/')]
+STATIC_URL = '/staticfiles/admin'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/admin')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/admin')]
 
 
 #debo borrar este codigo
