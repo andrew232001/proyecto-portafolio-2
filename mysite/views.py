df7668e6feac9cbc2324cd928872486902ebf91d
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 18:32:17 -0300

    primer template

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) :
-    return render(request,'staticfiles\Templates\index.html')
+    return render(request, 'staticfiles/Templates/index.html')
