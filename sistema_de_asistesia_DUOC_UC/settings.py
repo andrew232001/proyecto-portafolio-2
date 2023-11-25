e5f362d96cae5acfed5bb2f80ade09f2aeadba97
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 18:19:45 -0300

    primer template

--- sistema_de_asistesia_DUOC_UC/settings.py
+++ sistema_de_asistesia_DUOC_UC/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'sistema_de_asistesia_DUOC_UC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mysite\Templates')],
+        'DIRS': [os.path.join(BASE_DIR, '/app/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
