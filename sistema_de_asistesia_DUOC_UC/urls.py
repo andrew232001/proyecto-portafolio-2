7b1be65d92a995811b26a2112fb2da92c195233a
andrew232001 <andrew.quezada2001@gmail.com>
2023-11-25 01:20:47 -0300

    probando codigo de funcionamiento

--- sistema_de_asistesia_DUOC_UC/urls.py
+++ sistema_de_asistesia_DUOC_UC/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^staticfiles/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+
 ]
