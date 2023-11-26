1128ae4e6956f04ca8d2cb8fd649c96454d429b1
andrew232001 <85310450+andrew232001@users.noreply.github.com>
2023-11-25 21:27:19 -0300

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) :
-    return render(request, 'pepinollo/pepinollo/index.html')
+    return render(request, '/pepinollo/index.html')
